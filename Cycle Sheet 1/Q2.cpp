8ffecafb45601ba0e0e75ccfddd6c78f2fc78811
Ashwini Purohit <ashwinipurohit@yahoo.in>
2015-08-05 02:31:18 +0530

    Completed Q2. Works perfect.

--- Cycle Sheet 1/Q2.cpp
+++ Cycle Sheet 1/Q2.cpp
@@ -10,12 +10,27 @@ class account
 		float balance;
 		struct transactions
 		{
-			int transaction[5]={0};
-		};
+			int transaction[5];
+		}myTransactions;
 		void deposit();
 		void withdraw();
 		void display();
+		void displayAllTransactions();
 };
+void account::displayAllTransactions()
+{
+	for(int i=0;i<3;i++)
+	{	
+		cout<<"Account "<<i+1<<"'s details: \n";
+		for(int j=0;j<5;j++)
+		{
+		
+		cout<<myTransactions.transaction[j];
+	
+		}
+	}
+
+}
 
 void account::deposit()
 {
@@ -24,6 +39,17 @@ void account::deposit()
 	cin>>amount;
 	balance=balance+amount;
 	cout<<"Final balance :"<<balance;
+	int flag=0;
+	for(int j=0;j<5;j++)
+		{
+			if(myTransactions.transaction[j]==0&&flag!=1)
+			{
+			
+				myTransactions.transaction[j]=amount;		
+				flag=1;
+				break;
+			}
+		}
 
 }
 void account::withdraw()
@@ -33,11 +59,43 @@ void account::withdraw()
 	cin>>amount;
 	balance=balance-amount;
 	cout<<"Final balance :"<<balance;
+	int flag=0;
+	int amountNew=-amount;
+	for(int j=0;j<5;j++)
+		{
+			if(myTransactions.transaction[j]==0&&flag!=1)
+			{
+			
+				myTransactions.transaction[j]=amountNew;		
+				flag=1;
+				break;
+			}
+		}
+}
+void account::display()
+{
+	cout<<"Name :"<<name<<endl;
+	cout<<"Account Number: "<<accno;
+	cout<<"\nBalance: "<<balance;
 }
 
 int main()
 {
+	
+
 	account acct[3];
+// Initialize all the elements to 0 of structure
+for(int i=0;i<3;i++)
+	{
+		for(int j=0;j<5;j++)
+		{
+		
+		acct[i].myTransactions.transaction[j]=0;
+	
+		}
+	}
+
+	
 	for(int i=0;i<3;i++)
 	{
 		cout<<"Details of Person "<<i+1<<": \n";
@@ -68,6 +126,27 @@ int main()
 		if(acct[i].accno==accountNo)
 		{
 			acct[i].withdraw();
+		}
+	}
+	// To display the details of an account
+	cout<<"\nEnter Account number to display details: ";
+	cin>>accountNo;
+	for(int i=0;i<3;i++)
+	{
+		if(acct[i].accno==accountNo)
+		{
+			acct[i].display();
+		}
+	}
+	// To display all details
+	cout<<"\n Display all transactions :\n";
+	for(int i=0;i<3;i++)
+	{
+		for(int j=0;j<5;j++)
+		{
+		if(acct[i].myTransactions.transaction[j]!=0)
+		cout<<"Transaction "<<j+1<<" "<<acct[i].myTransactions.transaction[j]<<endl;
+	
 		}
 	}
 	
