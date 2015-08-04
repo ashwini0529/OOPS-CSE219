110c3d8da1268c2411ffb5fdfdd016279ef28732
Ashwini Purohit <ashwinipurohit@yahoo.in>
2015-08-04 21:59:51 +0530

    Question 2 started.

---
+++ Cycle Sheet 1/Q2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class account
+{
+	public:
+		
+		int accno;
+		char name[50];
+		float balance;
+		struct transactions
+		{
+			float transaction[5]={0};
+		}
+};
+
+void deposit()
+{
+	
+}
+
+int main()
+{
+	
+}
